4cb5f87303f451b3754d3eef2c9dc697a08a884b
Adam Hecht <adamo156@gmail.com>
2026-03-07 15:09:06 +0100

    Update colorscheme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -3,6 +3,7 @@ vim.pack.add({
 	"https://github.com/rebelot/kanagawa.nvim",
 	"https://github.com/jpwol/thorn.nvim",
 	"https://github.com/oskarnurm/koda.nvim",
+	"https://github.com/p00f/alabaster.nvim",
 })
 
 require("koda").setup({
@@ -53,9 +54,9 @@ require("kanagawa").setup({
 		return {}
 	end,
 	plugins = { markdown = true },
-	theme = "wave", -- Load "wave" theme
+	theme = "dragon", -- Load "wave" theme
 	background = { -- map the value of 'background' option to a theme
-		dark = "wave", -- try "dragon" !
+		dark = "dragon", -- try "dragon" !
 		light = "lotus",
 	},
 })
